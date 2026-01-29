ccef1f8b55a080f93abbb0173b680decb60ad872
lliangzeyuan-wq <lliangzeyuan@gmail.com>
2026-01-29 15:34:50 +0800

    分发饼干的代码实现

--- 力扣题目/力扣题目.cpp
+++ 力扣题目/力扣题目.cpp
@@ -1,2 +1,20 @@
-﻿#iunclude<iostream>
-return -
+﻿#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int index = s.size() - 1;
+        int result = 0;
+        for (int i = g.size()-1; i >= 0; i--) {
+            if (index >= 0 && s[index] >= g[i]) {
+                result++;
+                index--;
+            }
+        }
+        return result;
+    }
+};
