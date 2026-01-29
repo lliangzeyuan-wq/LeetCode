f24bd660524282c3aa0587b62395a10b18af25df
lliangzeyuan-wq <lliangzeyuan@gmail.com>
2026-01-29 21:59:06 +0800

    1

--- 力扣题目/力扣题目.cpp
+++ 力扣题目/力扣题目.cpp
@@ -1,19 +1,17 @@
 ﻿#include<iostream>
 using namespace std;
 #include<vector>
-#include<algorithm>
 class Solution {
 public:
-    int findContentChildren(vector<int>& g, vector<int>& s) {
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-        int index = s.size() - 1;
-        int result = 0;
-        for (int i = g.size()-1; i >= 0; i--) {
-            if (index >= 0 && s[index] >= g[i]) {
-                result++;
-                index--;
+    int maxSubArray(vector<int>& nums) {
+        int result = INT32_MIN;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            count += nums[i];
+            if (count > result) {
+                result=count;
             }
+            if (count <= 0) count = 0;
         }
         return result;
     }
